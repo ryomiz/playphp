edb16d01375fd4648a69f8e851e1b6f684ee634e
ryosuke mizuno <ryo.mizuno502@gmail.com>
2021-12-18 15:29:36 +0900

    [add] array

---
+++ www/html/bracket.php
@@ -0,0 +1,25 @@
+<?php
+$data = ["Alan", "Billie", "Charlie", "David", "Emilia"];
+
+// print $data[5];
+// print "\n";
+
+// $data[5] = "Farukko";
+// print $data[5];
+// print "\n";
+
+// $data[4] = "Farruko";
+// print $data[0];
+// print "\n";
+// print $data[1];
+// print "\n";
+// print $data[2];
+// print "\n";
+// print $data[3];
+// print "\n";
+// print $data[4];
+// print "\n";
+// print $data[5];
+// print "\n";
+
+print_r($data);
