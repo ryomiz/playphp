c89248cf0e8ce7e9761681b184f62c882eb07c7b
ryosuke mizuno <ryo.mizuno502@gmail.com>
2021-12-19 17:05:28 +0900

    [update] settings

--- www/html/index.php
+++ www/html/index.php
@@ -1,1 +1,13 @@
-<?php echo "PHP" ?>
+<?php
+
+const TAX = 1.1;
+$price = 1000;
+
+$msg = "Hello world";
+print "\n";
+print $msg;
+print "\n";
+print TAX * $price;
+print "\n";
+print "He's a \"good\" teacher";
+print "\n";
